f953cc74c2b0e6706f76d8499f184ca9b397b1f0
SatyamSatyarthi007 <156945085+SatyamSatyarthi007@users.noreply.github.com>
2024-06-30 07:25:28 +0530

    Add files via upload

---
+++ BMI_calculator.py
@@ -0,0 +1,6 @@
+height = input("Enter the height in m :")
+weight = input("Enter the weight in kg :")
+
+BMI = int(weight) / float((height*2))
+
+print("The Body mass index of your body is :" , int(BMI))
---
+++ Band_name_genrator.py
@@ -0,0 +1,4 @@
+print("Welcome to band name generator")
+city = input("Enter the name of your city : \n")
+favorite_thing= input("Enter the name of your dog : \n")
+print("The name of your band is : " , city + favorite_thing)
